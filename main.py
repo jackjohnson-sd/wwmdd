9ab096e17f41cb6aa06fe169bef67465d7405383
JackCarttronics <jack.johnson.pub@gmail.com>
2024-04-09 07:06:59 -0700

    Update main.py
    added player by playing time  chart

--- main.py
+++ main.py
@@ -3,10 +3,74 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-
+from datetime import datetime,timedelta
 
 START_SEASON = '2020-01-01'
 DB_FILENAME = "nba.sqlite"
+
+def plot3(game,title):
+
+    """
+    results = {  in seconds
+        'Josh' : [(START,LENGTH), (150,25) , (200,45)],
+        'Lu'  : [(10,60), (110,25) , (180,145)]
+    }
+    """
+   
+    playTimesbyPlayer = {}
+    sumByPlayer = {}
+    players = list(game[0].keys())
+
+    for player in players:
+        usage = game[0][player]
+
+        def timespantoSecs(a):
+            period = int(a[0][1]) - 1
+            game_clock = datetime.strptime('12:00','%M:%S') -  datetime.strptime(a[0][2], '%M:%S')
+            length = a[1]
+            start = period * 12 * 60 + game_clock.total_seconds()
+            return  (int(start),int(length))
+        
+        a = list(map(lambda x:timespantoSecs(x),usage))
+        b = sum(list(map(lambda x:x[1],a) ))
+        playTimesbyPlayer[player] = a
+        sumByPlayer[player] = b
+
+    def timeToString(t): return str(timedelta(seconds = t))[2:]    
+    team_minutes_played = list(map(lambda a :timeToString(a[1]),sumByPlayer.items()))
+    
+    labels = list(playTimesbyPlayer.keys())
+    data = list(playTimesbyPlayer.values())
+ 
+    figure, ax = plt.subplots(figsize=(9.2, 5))
+    ax.invert_yaxis()
+    ax.yaxis.set_visible(True)
+    ax.set_xlim(-25, (48 * 60) + 25)
+    ax.set_xticks([0,12*60,24*60,36*60,48*60],['','','','',''])
+    ax.grid(True,axis='x')
+    ax.set_title(title, fontsize=10)
+    ax.set_xlabel('periods')
+    ax.set_xticks([6*60, 18*60, 30*60,42*60], minor=True)
+    ax.set_xticklabels(['Q1','Q2','Q3','Q4'],minor=True)
+
+    for label in labels:
+        data = playTimesbyPlayer[label]
+        starts = list(map(lambda x:x[0],data))
+        widths = list(map(lambda x:x[1],data))
+        rects = ax.barh(label, widths, left=starts, align='edge', height=0.2)
+
+    y1, y2 = ax.get_ylim()
+    x1, x2 = ax.get_xlim()
+    ax2=ax.twinx()
+    ax2.set_ylim(y1, y2)
+
+    ax2.set_yticks( range(0,len(team_minutes_played)),team_minutes_played )
+    ax2.set_ylabel('minutes played')
+    ax2.set_xlim(x1, x2)
+
+    plt.tight_layout()
+    plt.show()
+    plt.close('all')
 
 def plot2(data):
         
@@ -100,13 +164,13 @@ def loadNBA():
 
         for chunk in pd.read_sql_query(query, db_con, chunksize=chunk_size,index_col= indexCol):
             chunks.append(chunk)
-            print('.',end = "")
-            count += chunk_size       
+            #print('.',end = "")
+            count += chunk_size
 
         _dfs[table_name] = pd.concat(chunks)
-        print(table_name, _dfs[table_name].shape)
+        #print(table_name, _dfs[table_name].shape)
 
-    print() 
+    print('LOAD COMPLETE') 
 
     # strip leading digit from season, its signifies pre, post and regular season
     _dfs['game']['season_id'] = _dfs['game']['season_id'].apply(lambda x:x[1:])
@@ -159,8 +223,8 @@ def filterGamesByDateRange(start, stop, games):
 
 def getTestData():
 
-    _START_DAY = '2023-03-21'
-    _STOP_DAY = '2023-03-22'
+    _START_DAY = '2023-03-01'
+    _STOP_DAY = '2023-03-31'
     _TEAM = 'OKC'
     _SEASON = '2022'
 
@@ -175,26 +239,101 @@ def dump(df,keepers):
             if t != None :
                 ts += f'{t} '
         print(ts)
-## firgithub
-def generatePBP():
 
-    keepers = [
-        'eventmsgtype', 
-        'period', 'pctimestring', 
-        #'homedescription', 
-        #'neutraldescription', 
-        #'visitordescription', 
+def dump_play_by_play(players,events,pbp):
+
+    for player in players:
+        print()
+        print(player,' -----------------')
+        subs = pbp[0].query(
+            f'(eventmsgtype == 8) and (player1_name == "{player}" or player2_name == "{player}")'
+            )
+        if subs.shape[0] == 0: print('dump_play_by_play() Error A', player, e.eventnum, e.game_id)
+        else:
+            for key,e in subs.iterrows():
+                print(
+                    f'{e.period} {e.pctimestring:<5} IN:{e.player2_name:<20} OUT:{e.player1_name:<20} {e.homedescription}'
+                )
+            print()
+            print()
+            
+def secDiff(start,stop):        
+
+    # flip clock time so its from the start of the period o.e starts at 00:00 vs 12:00
+    # add offset for diferences in periods
+    startPeriod = int(start[1]) - 1
+    startTime = datetime.strptime(start[2], '%M:%S') 
+    startDelta = datetime.strptime('12:00', '%M:%S') - startTime + timedelta(seconds = startPeriod * 12 * 60)
+
+    stopPeriod = int(stop[1]) - 1
+    stopTime = datetime.strptime(stop[2], '%M:%S')
+    stopDelta = datetime.strptime('12:00', '%M:%S') - stopTime + timedelta(seconds = stopPeriod * 12 * 60)
+
+    difference = stopDelta - startDelta
+
+    return int(difference.total_seconds())
+
+def totalTeamMinutes(starttime_duration_bydate, date):
+    total = 0
+    for key, b in starttime_duration_bydate[date][0].items():
+        tmin = sum(list(map(lambda x:x[1],b)))
+        total += tmin
+    return total
+ 
+def testPBPforErrors(starttime_duration_bydate):
+
+    for a in starttime_duration_bydate:
+        total = totalTeamMinutes(starttime_duration_bydate, a)
+        scoreErrors = starttime_duration_bydate[a][1]
         
-        'player1_name','player2_name','player3_name',]
-    
+        if total != 5*48*60:
+            print(f'{a} {"Team":>22}',timedelta(seconds =total),scoreErrors)
+            for key,b in starttime_duration_bydate[a][0].items():
+                for c in starttime_duration_bydate[a][0][key]:
+                    print(f'{key:<20} {str(c[0]):<20} {str(c[2]):<20} {str(timedelta(seconds=c[1]))}')
+                print()
+
+def getTimeSpansByPlayer(games,players):
+    _timeSpans = {}  # collect timespans played by this player
+    for player in players:
+        _timeSpans[player] = []
+        for x in games.index:
+            z = games.loc[x]
+            p1 = z['player1_name']
+            p2 = z['player2_name']
+            if (p1 == player) or (p2 == player):
+                t = 'IN' if p2 == player else "OUT"
+                _timeSpans[player].append([t,z.period,z.pctimestring]) 
+    return _timeSpans
+
+def generatePBP():
+   
     test_data, _start, _stop, _team, _season = getTestData()
  
     test_days = list(test_data.keys())
 
+    starttime_duration_bydate = {}
     for date in test_days:
+
+        starttime_duration_byPlayer = {}
+
         pbp_forDate = test_data[date].play_by_play[0]
         if pbp_forDate.shape[0] != 0:   
 
+            sc1 = pbp_forDate.query('eventmsgtype == 1 or eventmsgtype == 3')
+            scores = list(filter(lambda x:x != None,sc1.score))
+            score_errors = 0
+            for x,y in zip(scores,scores[1:]):
+                a = x.split(' - ')
+                b = y.split(' - ')
+                if a[0] == b[0]  :
+                    if a[1] == b[1]: 
+                        score_errors += 1
+                        #print('ERROR A',x,y)
+                else: 
+                    if a[1] != b[1]:
+                        score_errors += 1
+                        #print('ERROR B',x,y) 
             # 8 substitution event, get our players thata are subbed IN/OUT
             pbp_subs = pbp_forDate.query('eventmsgtype == 8') 
 
@@ -204,33 +343,61 @@ def generatePBP():
 
             playersInGame = list(set(p1s) | set(p2s))          
              
-            timeSpans = {}  # collect timespans played by this player
-            for player in playersInGame:
-                timeSpans[player] = []
-                for x in pbp_subs.index:
-                    z = pbp_subs.loc[x]
-                    p1 = z['player1_name']
-                    p2 = z['player2_name']
-                    if (p1 == player) or (p2 == player):
-                        t = 'IN' if p2 == player else "OUT"
-                        timeSpans[player].append([t,z.period,z.pctimestring]) 
-                        #print(date, z.period,
-                        #      f'{z.pctimestring:<6}', f'{x:<5}',
-                        #      ' IN:',f'{p2:<15}',' OUT:',f'{p1:<15}',
-                        #      z.homedescription  ) 
+            timeSpans = getTimeSpansByPlayer(pbp_subs,playersInGame)  # collect timespans played by this player
 
             for player in list(timeSpans.keys()):
-                print(player)
-                for _ts in timeSpans[player]:
+                _tss = timeSpans[player]
+                starttime_duration_byPlayer[player] = []
+
+                if len(_tss) > 0:
+
+                    # our problem of the minute is lineup
+                    # changes are not reported if they occur
+                    # during stopage at end of period. so fix it by ...
+
+                    # if first entry is OUT add IN at start of period
+                    if _tss[0][0] == 'OUT':
+                        _tss = [['IN',_tss[0][1],'12:00']] + _tss
+                    # if last entry is IN add OUT at end of period     
+                    if _tss[-1][0] == 'IN':
+                        _tss.append(['OUT',_tss[-1][1],'0:00'])
+
+                    # if we find two INs or OUTs together
+                    # insert IN or OUT bettween them at the begining of period
+                    insertsAdded = True    
+                    while insertsAdded:
+                        insertsAdded = False
+                        lenTss = len(_tss)
+                        for x in zip(range(0,lenTss),range(1,lenTss)):
+                            ts1 = _tss[x[0]]
+                            ts2 = _tss[x[1]]
+                            if ts2[0] == ts1[0]:  # both INs or OUTs
+                                if ts1[0] == 'IN':
+                                    newTimeStamp = ['OUT', ts1[1], '00:00']
+                                else: 
+                                    newTimeStamp = ['IN',ts2[1],'12:00']
+                                _tss.insert(x[1],newTimeStamp) 
+                                #print(player,x)
+                                insertsAdded = True
+                                break
                     
 
-                    sub_in = _ts[0] == 'IN'
-                    period = _ts[1]
-                    clock  = _ts[2]
+                    lenTss = len(_tss)
+                    for x,y in zip(range(0,lenTss,2),range(1,lenTss,2)):
+                        start_ts  = _tss[x]
+                        stop_ts   = _tss[y]
+                        if start_ts[0] == 'IN' and stop_ts[0] == 'OUT':
+                            ts = secDiff(start_ts,stop_ts)
+                            #ts_str = str(timedelta(seconds=ts))
+                            #print(f'{date} [{start_ts[0]:>2}, {start_ts[1]:>1}, {start_ts[2]:>5}], [{stop_ts[0]:>2}, {stop_ts[1]:>1}, {stop_ts[2]:>5}], {ts_str}')
 
-                    print(x,_ts[0],period,clock)
+                            starttime_duration_byPlayer[player].append([start_ts,ts,stop_ts])
+                        else:
+                            print('Error',player,x)   
+            
+            starttime_duration_bydate[date] = [starttime_duration_byPlayer,score_errors]
 
-    return
+    return starttime_duration_bydate
 
 def main():
 
@@ -241,8 +408,30 @@ def main():
 
     dfs, gamesByTeam = loadNBA()
 
+    start_duration_by_date = generatePBP()
 
-    generatePBP()
+    testPBPforErrors(start_duration_by_date)
+
+    dates = list(start_duration_by_date.keys())
+
+    for date in dates:
+        g = gamesByTeam['OKC']['2022'][date]
+        g.pts_home
+        g.pts_away
+        if g.matchup_home.split(' vs. ')[0] == 'OKC':
+            score = f'{int(g.pts_home)}-{int(g.pts_away)}'
+        else:
+            score = f'{int(g.pts_away)}-{int(g.pts_home)}'
+
+        total = totalTeamMinutes(start_duration_by_date, date)
+        t = str(timedelta(seconds=total)).split(':')
+        title = f'{g.matchup_home} {score} {date} {t[0]}:{t[1]} {g.game_id}'
+
+        if total != 48*60*5: 
+            players = list(start_duration_by_date[date][0].keys())
+            dump_play_by_play(players,[8],g.play_by_play)
+            plot3(start_duration_by_date[date], title)    
+
 
     """
     tests(test_data)

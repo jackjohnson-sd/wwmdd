e163145778d36f083eb09ca608b7565d6814d647
JackCarttronics <jack.johnson.pub@gmail.com>
2024-07-09 10:02:58 -0700

    test.sh and llm celanup

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
+
 sh w.sh -s web -team OKC -d 2024-04-03 -m plot -p stints
 sh w.sh -s csv -f llm_training_data/LALvOKC20240304.csv -m plot -p stints 
 sh w.sh -s csv -f llm_training_data/LALvOKC20240304.csv -m plot -p stints events
-sh w.sh -s csv -f llm_training_data/LALvOKC20240304.csv -m plot -p stints scores
+sh w.sh -s csv -f llm_training_data/LALvOKC20240304.csv -m plot -p stints score
 sh w.sh -s csv -f llm_training_data/LALvOKC20240304.csv -m plot -p stints boxscore
 sh w.sh -s csv -f llm_training_data/LALvOKC20240304.csv -m plot -p all
